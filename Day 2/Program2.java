7b05c5e03404e7b47a93711aa19f698ba9a733f5
Roohi-Ifroosooshai <34651878+Roohi-Ifroosooshai@users.noreply.github.com>
2021-05-25 20:17:27 +0530

    Update Program2.java

--- Day 2/Program2.java
+++ Day 2/Program2.java
@@ -1,56 +1,66 @@
 
 
-import java.io.*;
-import java.util.*;
-
-import java.lang.Thread;
-class Threads implements Runnable
-{
- 	Thread t;
-        int N;
-	Threads(int N)
-        {
-         this.N=N;
-         t=new Thread(this, "Thread");
-         System.out.println("New thread: " + t);
-         t.start();
-        }
-       public void run()
-        {
-	   try{
-            t.sleep(500);
-        }
-       catch(InterruptedException e)
-       {
-          System.out.println("Interrupted Exception");
-       }
-  }
+class Thread1 implements Runnable{
+   public void run() {
+       try {
+            for (int i = 1; i < 5; i++) {
+                  System.out.println("Thread1: "+i);
+            }
+    } catch (Exception e) {
+        e.getStackTrace();
+    }
+   }
 }
 
-  
-class program2
-{
-   public static void main(String arg[])
-    {
-     try{
-         Collection<Integer> list = new ArrayList<Integer>();
-        new Threads(1);
-            list.add(4);
-            list.add(8);
-            list.add(3);
-        new Threads(2);
-            list.add(8);
-            list.add(3);
-        new Threads(3);
-            list.add(5);
-            list.add(1);
-                 System.out.println(list);
-          Thread.sleep(1000);
-        
-     }
-     catch(InterruptedException e)
-      {
-        System.out.println("Interrupted Exception");
-      }
-  }
+class Thread2 implements Runnable{
+       public void run() {
+           try {
+                for (int i = 5; i < 8; i++) {
+                      System.out.println("Thread2: "+i);
+                }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+       }
+    }
+class Thread3 implements Runnable{
+       public  void run() {
+           try {
+                for (int i = 8; i <13; i++) {
+                      System.out.println("Thread3: "+i);
+                }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+       }
+    }
+
+public class Program2 {
+public static void main(String[] args) {
+    try {
+
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+        Thread3 thread3 = new Thread3();
+        Thread t1 = new Thread(thread1);
+        Thread t2 = new Thread(thread2);
+        Thread t3 = new Thread(thread3);
+        t1.start();
+
+        t1.join();
+
+        if( !t1.isAlive()) {
+            t2.start();
+        }
+
+        t2.join();
+
+        if (!t2.isAlive()) {
+            t3.start(); 
+        }
+
+    }catch(InterruptedException e) {
+        e.printStackTrace();
+    }
+}
 }
